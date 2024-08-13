46bfee06a8b6f49f3af719b095e9974cfd5e2c93
Erica Balderas <ericab40033@hotmail.com>
2024-08-12 19:21:52 -0600

    merging

--- server/server.js
+++ server/server.js
@@ -1,146 +1,80 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import Auth from '../utils/auth';
-import { ADD_USER } from '../utils/mutations';
-import { Form, Input, Button, Typography, Modal } from 'antd';
-import { UserOutlined, MailOutlined, LockOutlined } from '@ant-design/icons';
-import '../components/Cart/index'; // Ensure this path is correct based on your project structure
+require('dotenv').config();
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+const placesRoutes = require('./routes/api/placesRoutes');
+const bodyParser = require('body-parser');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+const { AuthenticationError } = require('./utils/auth');
 
-const { Title } = Typography;
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  formatError: (err) => {
+    console.log(err);
+    return err;
+  },
+});
 
-function Signup() {
-  const [formState, setFormState] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
+// Middleware
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Serve static assets
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [addUser, { data, error }] = useMutation(ADD_USER);
-
-  const handleFormSubmit = async (values) => {
-    console.log('Values: 23', values);
-    try {
-      const mutationResponse = await addUser({
-        variables: {
-          email: values.email,
-          password: values.password,
-          firstName: values.firstName,
-          lastName: values.lastName,
-        },
-      });
-      console.log('response data', data);
-      const token = mutationResponse.data.addUser.token;
-      Auth.login(token);
-      setIsModalVisible(true); // Show modal upon successful submission
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-
-  return (
-    <div className="container my-1">
-      <Link to="/login" className="ant-btn-secondary">
-        ← Go to Login
-      </Link>
-
-      <Title level={2} className="title" style={{ color: '#ffffff' }}>
-        Signup
-      </Title>
-      <Form
-        name="signup"
-        initialValues={{ remember: true }}
-        onFinish={handleFormSubmit}
-      >
-        <Form.Item
-          name="firstName"
-          rules={[{ required: true, message: 'Please input your first name!' }]}
-        >
-          <Input
-            prefix={<UserOutlined />}
-            placeholder="First Name"
-            name="firstName"
-            value={formState.firstName}
-            onChange={handleChange}
-          />
-        </Form.Item>
-        <Form.Item
-          name="lastName"
-          rules={[{ required: true, message: 'Please input your last name!' }]}
-        >
-          <Input
-            prefix={<UserOutlined />}
-            placeholder="Last Name"
-            name="lastName"
-            value={formState.lastName}
-            onChange={handleChange}
-          />
-        </Form.Item>
-        <Form.Item
-          name="email"
-          rules={[{ required: true, message: 'Please input your email!' }]}
-        >
-          <Input
-            prefix={<MailOutlined />}
-            placeholder="Email"
-            name="email"
-            type="email"
-            value={formState.email}
-            onChange={handleChange}
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input
-            prefix={<LockOutlined />}
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={formState.password}
-            onChange={handleChange}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-      {error && <div>Signup failed</div>}
-
-      <Modal
-        title="Success"
-        open={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <img
-          src="https://via.placeholder.com/400"
-          alt="Success"
-          style={{ width: '100%' }}
-        />
-      </Modal>
-    </div>
-  );
 }
 
-export default Signup;
+// API Routes
+app.use('/api/places', placesRoutes);
+
+// Start Apollo Server
+const startApolloServer = async () => {
+  await server.start();
+
+  app.use(
+    '/graphql',
+    expressMiddleware(server, {
+      context: authMiddleware,
+    })
+  );
+
+  // app.use('/graphql', expressMiddleware(server, {
+  //   context: ({ req }) => {
+  //     const token = req.headers.authorization || '';
+  //     if (token) {
+  //       try {
+  //         const { data } = jwt.verify(token.split(' ').pop().trim(), process.env.JWT_SECRET, { maxAge: '2h' });
+  //         return { user: data };
+  //       } catch (err) {
+  //         console.error('Invalid token');
+  //         throw new AuthenticationError();
+  //       }
+  //     }
+  //     return {};
+  //   }
+  //  secret
+  // }));
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+// Call the async function to start the server
+startApolloServer();
