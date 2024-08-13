e746ee0f8c78b36e41993c7619a92dc81419b4e3
Laplace <lap2837@outlook.com>
2024-08-12 20:37:25 -0600

    added maps to front page

--- client/src/components/CategoryMenu/index.jsx
+++ client/src/components/CategoryMenu/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useQuery } from '@apollo/client';
 import { useStoreContext } from '../../utils/GlobalState';
 import {
@@ -10,10 +10,11 @@ import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
   const [state, dispatch] = useStoreContext();
-
   const { categories } = state;
-
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+  
+  // Create a reference for the map container
+  const mapRef = useRef(null);
 
   useEffect(() => {
     if (categoryData) {
@@ -33,6 +34,16 @@ function CategoryMenu() {
       });
     }
   }, [categoryData, loading, dispatch]);
+
+  useEffect(() => {
+    // Initialize the Google Map
+    if (mapRef.current) {
+      const map = new window.google.maps.Map(mapRef.current, {
+        center: { lat: -34.397, lng: 150.644 }, // Set the initial center of the map
+        zoom: 8,
+      });
+    }
+  }, []);
 
   const handleClick = (id) => {
     dispatch({
@@ -61,8 +72,15 @@ function CategoryMenu() {
       >
         All
       </button>
+      
+      {/* Add a div for the Google Map */}
+      <div
+        ref={mapRef}
+        style={{ width: '100%', height: '400px', marginTop: '20px' }}
+      ></div>
     </div>
   );
 }
 
 export default CategoryMenu;
+
